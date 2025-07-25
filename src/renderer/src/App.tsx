1fdce0d490fb2584d05da3affc15620093d06efc
bienfait shomari <bienfaitshm@gmail.com>
2025-07-25 15:56:16 +0200

    fix(ui): correction des imports

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -2,11 +2,21 @@ import { ThemeProvider } from "@/renderer/providers/theme";
 import Router from "@/renderer/screens/router";
 import QuerieProvider from "@/renderer/libs/queries/providers";
 import { Toaster } from "@/renderer/components/ui/toaster";
+import { SidebarProvider, SidebarTrigger } from "@/renderer/components/ui/sidebar"
+import { AppSidebar } from "@/renderer/components/app-sidebar"
+
 function App(): JSX.Element {
   return (
     <QuerieProvider>
       <ThemeProvider>
-        <Router />
+        <SidebarProvider>
+          <AppSidebar />
+          <main>
+            <SidebarTrigger />
+            <Router />
+          </main>
+        </SidebarProvider>
+
         <Toaster />
       </ThemeProvider>
     </QuerieProvider>
