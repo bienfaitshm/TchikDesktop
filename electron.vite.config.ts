6a2d899f4fec3396613ecc667d1fca7ca99ba0c8
bienfait shomari <bienfaitshm@gmail.com>
2025-07-26 07:43:57 +0200

    refactore(files): changement de la structure de fichiers

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,27 +1,27 @@
-import { resolve } from "path"
-import { defineConfig, externalizeDepsPlugin } from "electron-vite"
-import react from "@vitejs/plugin-react"
+import { resolve } from "path";
+import { defineConfig, externalizeDepsPlugin } from "electron-vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-    main: {
-        resolve: {
-            alias: {
-                "@/main": resolve("src/main"),
-                "@/camons": resolve("src/camons"),
-            },
-        },
-        plugins: [externalizeDepsPlugin()],
+  main: {
+    resolve: {
+      alias: {
+        "@/main": resolve("src/main"),
+        "@/camons": resolve("src/camons"),
+      },
     },
-    preload: {
-        plugins: [externalizeDepsPlugin()],
+    plugins: [externalizeDepsPlugin()],
+  },
+  preload: {
+    plugins: [externalizeDepsPlugin()],
+  },
+  renderer: {
+    resolve: {
+      alias: {
+        "@/renderer": resolve("src/renderer"),
+        "@/camons": resolve("src/camons"),
+      },
     },
-    renderer: {
-        resolve: {
-            alias: {
-                "@/renderer": resolve("src/renderer/src"),
-                "@/camons": resolve("src/camons"),
-            },
-        },
-        plugins: [react()],
-    },
-})
+    plugins: [react()],
+  },
+});
