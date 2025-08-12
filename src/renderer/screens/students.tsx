442934ff1118bedfa68894209af3a2ceac8fe60e
bienfait shomari <bienfaitshm@gmail.com>
2025-08-12 13:18:45 +0200

    feat(ui): ajout  du view classroom pour les eleves

--- src/renderer/screens/students.tsx
+++ src/renderer/screens/students.tsx
@@ -1,71 +1,77 @@
 import { WithSchoolAndYearId } from "@/camons/types/services"
-import { TypographyH1 } from "../components/ui/typography"
-import { useGetEnrolements } from "@/renderer/libs/queries/enrolement"
-import { useGetCurrentYearSchool } from "../libs/stores/app-store"
+import { TypographyH2, TypographyH3, TypographyP, TypographySmall } from "@/renderer/components/ui/typography"
+import { Shapes } from "lucide-react"
+import {
+    Tabs,
+    TabsContent,
+    TabsList,
+    TabsTrigger,
+} from "@/renderer/components/ui/tabs"
+import { useGetCurrentYearSchool } from "@/renderer/libs/stores/app-store"
+import { useGetClassrooms } from "@/renderer/libs/queries/school"
+import { SECTION, SECTION_TRANSLATIONS } from "@/camons/constants/enum"
+import { getEnumKeyValueList } from "@/camons/utils"
+import { Badge } from "@/renderer/components/ui/badge"
+import { Link } from "react-router"
 
-const StudentEnrolement: React.FC<Required<WithSchoolAndYearId<{}>>> = ({ schoolId, yearId }) => {
-    const { data: infos, isLoading } = useGetEnrolements({ schoolId, yearId })
 
-    if (isLoading) {
-        return <div>Chargement...</div>;
-    }
+type CurrentYearSchoolProps<T extends {} = {}> = Required<WithSchoolAndYearId<T>>
 
-    // Assurez-vous que les données ne sont pas nulles avant de les utiliser
-    const students = infos || [];
 
+const SECTIONS_LISTS = getEnumKeyValueList(SECTION, SECTION_TRANSLATIONS)
+
+const ClassroomSection: React.FC<CurrentYearSchoolProps<{ section: SECTION }>> = ({ schoolId, section, yearId }) => {
+    const { data: classrooms = [] } = useGetClassrooms(schoolId, yearId)
     return (
-        <div className="flex">
-            {/* Side - Barre latérale */}
-            <div className="w-1/4 p-4 border-r overflow-y-auto sticky top-0">
-                <h2 className="text-xl font-bold mb-4">Liste des élèves</h2>
-                <ul className="space-y-2">
-                    {students.length > 0 ? (
-                        students.map((student) => (
-                            <li key={student.enrolementId} className="p-2 border rounded-md cursor-pointe">
-                                {student.code}
-                            </li>
-                        ))
-                    ) : (
-                        <p>Aucun élève inscrit pour l'instant.</p>
-                    )}
-                </ul>
-            </div>
+        <div className="grid grid-cols-3 gap-5 mt-5">
+            {classrooms.map(classroom => (
+                <Link to="#" className="rounded-xl cursor-pointer p-4 bg-accent-foreground/5 hover:bg-accent-foreground/10 transition-colors">
+                    <div className="bg-primary rounded-full p-2 w-fit">
+                        <Shapes className="text-white size-8" />
+                    </div>
+                    <TypographyH3 className="mb-0 text-lg">{classroom.shortIdentifier}</TypographyH3>
+                    <TypographyP className="text-sm text-muted-foreground">{classroom.identifier}</TypographyP>
+                </Link>
+            ))}
+        </div>
+    )
+}
 
-            {/* Main - Contenu principal */}
-            <div className="flex-1 p-8 overflow-y-auto">
-                <TypographyH1>
-                    Détails de l'élève
-                </TypographyH1>
-                <div className="mt-4">
-                    {/* Ici, vous afficherez les détails de l'élève sélectionné */}
-                    <p>Sélectionnez un élève dans la liste pour voir ses détails.</p>
-                    <code>
-                        {JSON.stringify(infos, null, 4)}
-                    </code>
-                    <code>
-                        {JSON.stringify(infos, null, 4)}
-                    </code>
-                    <code>
-                        {JSON.stringify(infos, null, 4)}
-                    </code>
-                    <code>
-                        {JSON.stringify(infos, null, 4)}
-                    </code>
-                    <code>
-                        {JSON.stringify(infos, null, 4)}
-                    </code>
-                </div>
+const ClassroomGrid: React.FC<CurrentYearSchoolProps> = ({ schoolId, yearId }) => {
+    return (
+        <div className="container mx-auto px-4 py-8 max-w-screen-lg space-y-10">
+            <div>
+                <Badge className="mb-2">Élèves</Badge>
+                <TypographyH2 className="mb-2">Listes des classes disponibles</TypographyH2>
+                <TypographySmall>
+                    Total des élèves inscrits pour cette année est <b className="font-semibold">1000</b>{" "}
+                </TypographySmall>
+            </div>
+            <div>
+                <Tabs defaultValue={SECTION.SECONDARY}>
+                    <TabsList className="rounded-full mb-4">
+                        {SECTIONS_LISTS.map(section => (
+                            <TabsTrigger className="rounded-full" key={section.key} value={section.value}>{section.label}</TabsTrigger>
+                        ))}
+                    </TabsList>
+                    {SECTIONS_LISTS.map(section => (
+                        <TabsContent key={section.key} value={section.value}>
+                            <ClassroomSection section={section.value} schoolId={schoolId} yearId={yearId} />
+                        </TabsContent>
+                    ))}
+                </Tabs>
             </div>
         </div>
     )
 }
+
 
 const StudentScreen = () => {
     const { schoolId, yearId } = useGetCurrentYearSchool()
     if (!schoolId && !yearId) {
         return null;
     }
-    return <StudentEnrolement schoolId={schoolId} yearId={yearId} />
+    return <ClassroomGrid schoolId={schoolId} yearId={yearId} />
 }
 
 export default StudentScreen
